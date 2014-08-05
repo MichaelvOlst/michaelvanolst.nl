53e6e2c43b4badb17250e5be8fed7dcacbb98781
Michael <michaeltje@live.nl>
2014-08-05 13:31:54 +0200

    deleted some files and re arranged them

--- app/views/layouts/nav.blade.php
+++
@@ -1,8 +0,0 @@
-
-<nav class="main-nav">
-	<ul class="nav">
-		@foreach ( $pages as $page)
-			<li> {{ link_to($page->pages, $page->title, ['class' => set_active($page->pages)] ) }}</li>
-		@endforeach
-	 </ul>
-</nav>
